f3150eb433a00354cfed7ea1cc78dd5f439742f2
Ray1014mo <2783663507@qq.com>
2024-04-29 18:54:19 +0800

    delete license

--- app/thirdparty/java/other/com/vladsch/flexmark/ext/katex/FlexmarkKatexExtension.java
+++ app/thirdparty/java/other/com/vladsch/flexmark/ext/katex/FlexmarkKatexExtension.java
@@ -1,34 +1,3 @@
-/*##############################################################################
- *
- * Copyright (c) 2015-2016, Atlassian Pty Ltd
- * All rights reserved.
- *
- * Copyright (c) 2016-2018, Vladimir Schneider,
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * * Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- *
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *#############################################################################*/
-
 package other.com.vladsch.flexmark.ext.katex;
 
 import com.vladsch.flexmark.ast.DelimitedNode;
@@ -60,21 +29,8 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/* This extension is based on Flexmark's GitLab Flavoured Markdown Extension[1].
- * The sources [2] were stripped of everything not relevant to math parsing.
- *
- * The extension implements parsers for inline math mode, `$...$`, and for display
- * math mode, `$$...$$`. Both are based on `internal/GitLabInlineMathParser.java`,
- * i.e. both are InlineParsers, not BlockParsers.
- *
- * [1]: https://github.com/vsch/flexmark-java/wiki/Extensions#gitlab-flavoured-markdown
- * [2]: https://github.com/vsch/flexmark-java/tree/0.42.14/flexmark-ext-gitlab/src/main/java/com/vladsch/flexmark/ext/gitlab
- */
 public class FlexmarkKatexExtension {
 
-    /**
-     * Extension for KaTeX
-     */
     public static class KatexExtension implements Parser.ParserExtension, HtmlRenderer.HtmlRendererExtension {
         public static final DataKey<Boolean> INLINE_MATH_PARSER = new DataKey<>("INLINE_MATH_PARSER", true);
         public static final DataKey<Boolean> DISPLAY_MATH_PARSER = new DataKey<>("DISPLAY_MATH_PARSER", true);
--- gradlew
+++ gradlew
@@ -1,31 +1,7 @@
 #!/usr/bin/env sh
 
-#
-# Copyright 2015 the original author or authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
-##############################################################################
-##
-##  Gradle start up script for UN*X
-##
-##############################################################################
-
-# Attempt to set APP_HOME
-# Resolve links: $0 may be a link
 PRG="$0"
-# Need this for relative symlinks.
 while [ -h "$PRG" ] ; do
     ls=`ls -ld "$PRG"`
     link=`expr "$ls" : '.*-> \(.*\)$'`
@@ -43,10 +19,8 @@ cd "$SAVED" >/dev/null
 APP_NAME="Gradle"
 APP_BASE_NAME=`basename "$0"`
 
-# Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 DEFAULT_JVM_OPTS='"-Xmx64m" "-Xms64m"'
 
-# Use the maximum available, or set MAX_FD != -1 to use that value.
 MAX_FD="maximum"
 
 warn () {
@@ -60,7 +34,6 @@ die () {
     exit 1
 }
 
-# OS specific support (must be 'true' or 'false').
 cygwin=false
 msys=false
 darwin=false
@@ -83,10 +56,8 @@ esac
 CLASSPATH=$APP_HOME/gradle/wrapper/gradle-wrapper.jar
 
 
-# Determine the Java command to use to start the JVM.
 if [ -n "$JAVA_HOME" ] ; then
     if [ -x "$JAVA_HOME/jre/sh/java" ] ; then
-        # IBM's JDK on AIX uses strange locations for the executables
         JAVACMD="$JAVA_HOME/jre/sh/java"
     else
         JAVACMD="$JAVA_HOME/bin/java"
